e2fe8ed6358fa0b9e3fd3120fd24db2ac537450f
YJ <myiosemail@126.com>
2017-03-24 11:14:37 +0800

    Update AppDelegate.m
    添加Block 回调

--- YJLaunchImage/AppDelegate.m
+++ YJLaunchImage/AppDelegate.m
@@ -10,6 +10,7 @@
 #import "YJTabBarController.h"
 #import "YJLaunchViewManager.h"
 #import "YJAdModel.h"
+#import "YJAdViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -38,32 +39,13 @@
     YJLaunchViewManager *launchController = [YJLaunchViewManager launchViewManger];
     launchController.adModel=adModel;
     [launchController showView:rootViewController.view];
+     __weak typeof(self)weakSelf=self;
+    launchController.tapClick=^{
+        YJAdViewController * adVc=[[YJAdViewController alloc]init];
+        adVc.adModel=adModel;
+        adVc.hidesBottomBarWhenPushed=YES;
+        [weakSelf.window.rootViewController.childViewControllers[0] pushViewController:adVc animated:YES];
+    };
 }
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
-
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:
 @end
